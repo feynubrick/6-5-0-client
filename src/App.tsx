bfe1f651a4caf1eb0319c88cfcb0d4812698e52f
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-08 22:52:34 +0900

    Revert "added layout for home page"
    This reverts commit 1b2024e34ce8206ab0ce9f98a9af8cdd85f8df2c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import Home from './pages/Home';
-
 function App() {
-  return <Home />;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
--- src/components/SidePanelButton.tsx
+++
@@ -1,9 +0,0 @@
-interface IProps {
-  text: string;
-}
-
-function SidePanelButton({ text }: IProps) {
-  return <button>{text}</button>;
-}
-
-export default SidePanelButton;
