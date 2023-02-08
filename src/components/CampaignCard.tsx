92c5e1798cd94ae9340963b149f058443bec5929
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-08 22:52:27 +0900

    Revert "캠페인 카드 기본 배치"
    This reverts commit 0b6ac6cae8d0049aa0b701f2dfa20f5bd14df3b6.

--- src/components/CampaignCard.tsx
+++
@@ -1,5 +0,0 @@
-function CampaignCard() {
-  return <div className="bg-white h-48"></div>;
-}
-
-export default CampaignCard;
--- src/components/CampaignViewer.tsx
+++ src/components/CampaignViewer.tsx
@@ -1,18 +1,6 @@
-import CampaignCard from './CampaignCard';
 function CampaignViewer() {
   return (
-    <div className="h-full bg-red-200 border-2 border-red-800 grid grid-cols-4 gap-4 content-start">
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-      <CampaignCard></CampaignCard>
-    </div>
+    <div className="h-full bg-red-200 border-2 border-red-800">캠페인 뷰어</div>
   );
 }
 
