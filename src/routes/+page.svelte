2f196bf5a7d95f8c42337a6729e6802325726719
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-10 22:55:11 +0900

    svelte & tailwindcss로 프로젝트 시작 (#1)
    * initialize svelte with sveltekit
    
    * add tailwindcss to svelte
    
    * 타이틀 바와 내용 카드 목록 만들기

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="">
+	<header class="sticky top-0 z-50 w-full bg-slate-800 h-12 text-white p-1">
+		<p class="align-middle">타이틀</p>
+	</header>
+	<div class="flex flex-col justify-items-center space-y-1 p-1">
+		<div class="bg-slate-200 text-center rounded-lg h-32">아이템1</div>
+		<div class="bg-slate-200 text-center rounded-lg h-32">아이템2</div>
+		<div class="bg-slate-200 text-center rounded-lg h-32">아이템3</div>
+		<div class="bg-slate-200 text-center rounded-lg h-32">아이템4</div>
+		<div class="bg-slate-200 text-center rounded-lg h-32">아이템5</div>
+	</div>
+</div>
