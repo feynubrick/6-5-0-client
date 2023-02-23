1bbf32b35eda736f8b85633dbb7078d954037887
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-23 23:35:12 +0900

    preload시 에러나지 않도록  링크를 / 에서 /jobs로 변경

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -5,6 +5,7 @@
 <header
 	class="sticky top-0 z-30 w-full bg-slate-800 text-white p-2 drop-shadow-xl flex justify-between h-12"
 >
-	<a href="/" class="align-middle text-center text-2xl font-semibold">Joyfluencer</a>
+	<!-- data-sveltekit-preload-data="tap" 을 쓰면 tap 했을 때만 preload 한다 -->
+	<a href="/jobs" class="align-middle text-center text-2xl font-semibold">Joyfluencer</a>
 	<DropDownMenu />
 </header>
