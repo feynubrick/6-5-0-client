232de13263edf6c05ba12b708a6f34e8ef1b136e
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-24 23:51:01 +0900

    로고 헤더에 추가

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -2,10 +2,17 @@
 	import DropDownMenu from './DropDownMenu.svelte';
 </script>
 
-<header
-	class="sticky top-0 z-30 w-full bg-slate-800 text-white p-2 drop-shadow-xl flex justify-between h-12"
->
-	<!-- data-sveltekit-preload-data="tap" 을 쓰면 tap 했을 때만 preload 한다 -->
-	<a href="/jobs" class="align-middle text-center text-2xl font-semibold">Joyfluencer</a>
-	<DropDownMenu />
+<header class="sticky top-0 z-30 w-full bg-slate-800 text-white p-2 drop-shadow-xl h-12 ">
+	<div class="flex flex-row h-full content-start">
+		<!-- data-sveltekit-preload-data="tap" 을 쓰면 tap 했을 때만 preload 한다 -->
+		<div class="flex justify-start">
+			<a href="/jobs">
+				<img src="/short-logo-red.png" alt="short-logo-red" class="h-8" />
+			</a>
+			<p class="align-middle text-left text-2xl font-semibold">Joyfluencer</p>
+		</div>
+		<div class="grow flex justify-end">
+			<DropDownMenu />
+		</div>
+	</div>
 </header>
